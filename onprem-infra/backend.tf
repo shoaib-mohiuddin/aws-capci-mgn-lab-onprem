65819ed21e52e3992e2d2171e57d9fd025f79d86
shoaib-mohiuddin <shoaib.mohiuddin@cloudreach.com>
2023-06-20 14:50:50 +0530

    Updates backend to reflect repo structure

--- onprem-infra/backend.tf
+++ onprem-infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "capci-mgn-lab-tfstates"
-    key            = "capci-mgn-lab/on-prem-vpc/terraform.tfstates"
+    key            = "aws-capci-mgn-lab-onprem/onprem-infra/terraform.tfstates"
     region         = "us-west-2"
     dynamodb_table = "terraform-lock-capci-mgn-lab"
   }
